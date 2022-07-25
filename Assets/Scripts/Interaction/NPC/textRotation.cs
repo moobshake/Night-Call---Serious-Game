2bd99e52a208a16fadf133cf3d53e4ce1aa236d7
huijuank <61876615+huijuank@users.noreply.github.com>
2022-07-26 02:21:31 +0800

    Fixed text rotation based on camera

--- Assets/Scripts/Interaction/NPC/textRotation.cs
+++ Assets/Scripts/Interaction/NPC/textRotation.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class textRotation : MonoBehaviour
 {
-    public Transform player;
+    public Transform camera1;
+    public Transform camera2;
     void Update()
     {
-        transform.rotation = player.rotation;
+        if (Display.activeEditorGameViewTarget == 0) {
+            transform.rotation =camera1.rotation;
+        }
+        else if (Display.activeEditorGameViewTarget == 1) {
+            transform.rotation =camera2.rotation;
+        }
+        
     }
 }
